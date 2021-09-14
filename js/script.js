160bfd54251a8a798d8b71cdbad25b1cd0ff6ba2
jwschildt <87144483+jwschildt@users.noreply.github.com>
2021-09-14 12:19:33 -0700

    updated quotes.

--- js/script.js
+++ js/script.js
@@ -3,20 +3,50 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
+// For assistance:
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
+/***
+ * `quotes` array
 ***/
-
-
+const quotes = [
+  {
+  quote: 'You know as well as I do that fear only exists for one purpose... to be conquered.',
+  source: 'Captain Katherine Janeway',
+  citation: 'Star Trek: Voyager',
+  year: '1996'
+},
+{
+  quote: "There is a way out of every box, a solution to every puzzle; it's just a matter of finding it.",
+  source: 'Captain Jean-Luc Picard',
+  citation: 'Star Trek: The Next Generation',
+  year: '1993, 47304.2'
+},
+{
+  quote: 'Of course it is happening inside your head, Harry, but why on earth should that mean it is not real?',
+  source: 'Albus Dumbledore',
+  citation: 'Harry Potter and the Deathly Hallows',
+  year: '2007'
+},
+{
+  quote: 'Things we lose have a way of coming back to us in the end, if not always in the way we expect',
+  source: 'Luna Lovegood',
+  citation: 'Harry Potter and the Order of the Phoenix',
+  year: '2003'
+},
+{
+  quote: "I solemnly swear I am up to no good.",
+  source: 'Harry Potter',
+  citation: 'Harry Potter and the Prisoner of Azkaban',
+  year: '2004'
+}
+];
 
 /***
  * `getRandomQuote` function
 ***/
-
+//function getRandomQuote()
 
 
 /***
